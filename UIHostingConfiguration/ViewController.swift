11d587857cd1009731939865af0ec27401916e13
TDCIAN <tdcian71@gmail.com>
2022-06-28 22:00:38 +0900

    finished iOSAcademy's example

--- UIHostingConfiguration/ViewController.swift
+++ UIHostingConfiguration/ViewController.swift
@@ -6,26 +6,68 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
     
-    let collectionView: UICollectionView = {
+    private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = .zero
         layout.itemSize = CGSize(width: 320, height: 100)
         layout.minimumInteritemSpacing = 1
         layout.minimumLineSpacing = 1
-        let collectionView = UICollectionView(frame: .zero)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = .systemBackground
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
         return collectionView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if #available(iOS 16.0, *) {
+            cell.contentConfiguration = UIHostingConfiguration {
+                HStack {
+                    Image(systemName: "star")
+                    VStack(alignment: .leading) {
+                        Text("Favorites")
+                            .font(.title)
+                        Text("See all your liked content!")
+                            .font(.body)
+                    }
+                    .padding()
+                    
+                    Spacer()
+                }
+            }
+        } else {
+            // Configure cell for under iOS 16.0
+        }
+        
+        cell.backgroundColor = .systemPink
+        return cell
+    }
+}
+
+final class FooCell: UICollectionViewCell {
+    weak var delegate: FooCellDelegate?
+}
